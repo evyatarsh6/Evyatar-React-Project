06845b2abba1120b205eab6dadb81e4bfff156ce
evyatarsh6 <evyash6@gmail.com>
2023-11-05 08:21:01 +0200

    continue with the reducer function

--- src/reducers/handleTODOSReducer.js
+++ src/reducers/handleTODOSReducer.js
@@ -1,16 +1,72 @@
 import React from "react"
 import { useState } from "react"
 import { useId } from "react";
+import { searchForWorkspaceRoot } from "vite";
 
 
-export const addTODOReducer = (missions, { newTODO: { TODOKind ="calling his mother", TODOID: newTODOID }} ) => {
 
-    const singleMission = { newTODO.TODOID : { kind : newTODO.TODOKind, isChoosen: false, isDeleted : false }}
-    const action = {"type": "addTODO", "actionProps" : {"newTODOMissionid": newMissionid ,"newTODOInfo": singleMission}}}
+let newMissionid = '12'
+let singleMission = { newTODO.TODOID : { kind : newTODO.TODOKind, isChoosen: false, isDeleted : false }}
+const actionexample = {type: "addTODO", actionProps : {"newTODOMissionid": newMissionid ,"newTODOInfo": singleMission}}
 
-const addTODO = (state, action) => {
+    export const TODOReducer = (state, action) =>  {
 
-    setMissions(...missions, action["actionProps"]["newTODOMissionid"]: { action["newTODOInfo"].newMissionid } )
-}
+        switch (action.type) {
+            case "addTODO":
+                return {
+                    ...state,
+                    [action.ID]: {
+                        kind: newTODO.TODOKind,
+                        isChoosen: false,
+                        isDeleted: false
+                    }
+                };
 
-return  missions
+            case "deleteTODO":
+                return {
+                    ...state, 
+                    [action.ID]:
+                    isDeleted = true
+                };
+
+                
+            // const singleMission = { newTODO.TODOID : { kind : newTODO.TODOKind, isChoosen: false, isDeleted : false }}
+                
+        
+            // case "filterTODOS":
+            //     return {
+            //         ...state,
+            //         [Object.keys(state).forEach($elem => {
+            //             if (state[$elem].isChoosen) {
+                            
+            //             }
+            //         })]
+                       
+                    
+
+                // }
+
+            default:
+                return state; 
+        }
+    
+    }
+    
+    
+    
+
+
+
+
+
+//     const singleMission = { newTODO.TODOID : { kind : newTODO.TODOKind, isChoosen: false, isDeleted : false }}
+
+//     const action = {type: "addTODO", actionProps : {"newTODOMissionid": newMissionid ,"newTODOInfo": singleMission}}
+
+// const addTODO = (state, action) => {
+
+
+    // return  (...missions, action["actionProps"]["newTODOMissionid"]: { action["newTODOInfo"].newMissionid } )
+
+    // setMissions(...missions, action["actionProps"]["newTODOMissionid"]: { action["newTODOInfo"].newMissionid } )
+// }

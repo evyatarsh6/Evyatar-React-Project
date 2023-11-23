6a840eee721378a2a98ac237038e2c94f195b39f
evyatarsh6 <evyash6@gmail.com>
2023-11-23 14:50:56 +0200

    avi

--- src/containers/OpenLayersMap/MapContainer.jsx
+++ src/containers/OpenLayersMap/MapContainer.jsx
@@ -1,23 +1,11 @@
-import GeoJSON from 'ol/format/GeoJSON';
+// import GeoJSON from 'ol/format/GeoJSON';
 import Map from 'ol/Map';
-import VectorLayer from 'ol/layer/Vector';
-import VectorSource from 'ol/source/Vector';
 import TileLayer from 'ol/layer/Tile';
 import View from 'ol/View';
 import OSM from 'ol/source/OSM.js';
+import Control from 'ol/control/Control';
 
-// new VectorLayer({
-//     source: new VectorSource({
-//       format: new GeoJSON(),
-    //   url: geojsonData,
-// new TileLayer({source: new OSM()}),
-        // new VectorLayer({
-        //     source: new VectorSource({
-        //       format: new GeoJSON(),
-        //       url: './data/countries.json',
-        //     // url: 'C:/Users/evyas/OneDrive/Documents/GitHub/Evyatar-React-Project/src/data/countries.json'
-        //     }),
-        //   }),
+
 new Map({
 
     layers: [
@@ -28,13 +16,16 @@ new Map({
         center: [0, 0],
         zoom:  1,
     }),
-    target: 'map',
+    target: 'map', 
 });
+
+// var scaleLineControl = new ol.control.ScaleLine();
+// Map.addControl(scaleLineControl );
 
 export const MapContainer = () => {
     
     return (
-        <div id="map" style={{
+        <div id="map" style={{ 
             margin: "0",
             width: "100%",
             fontFamily:" sans-serif",

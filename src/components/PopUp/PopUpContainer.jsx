65d278d20d57b9a6300352999d0dc1761a2ee387
evyatarsh6 <evyash6@gmail.com>
2023-12-24 16:25:46 +0200

    update folder structure

--- src/components/PopUp/PopUpContainer.jsx
+++ src/components/PopUp/PopUpContainer.jsx
@@ -1,11 +1,13 @@
 import { PopUpContent } from "./PopUpContent"
 
 export const PopUp = () => {
-
-    return (
-        <div id="popup" className="ol-popup">
-            <a href="#" id="popup-closer" className="ol-popup-closer"/>
-              <PopUpContent/>
-        </div>
-    )
+    const avi = true
+    if (!avi) {
+        return (
+            <div id="popup" className="ol-popup">
+                <a href="#" id="popup-closer" className="ol-popup-closer"/>
+                  <PopUpContent/>
+            </div>
+        )
+    }
 }
--- src/rootReducer.js
+++ src/rootReducer.js
@@ -2,18 +2,17 @@ import {combineReducers} from "redux";
 import { TODOS } from "./reducers/uiReducer/handleTODOSReducer";
 import { filterKind } from "./reducers/uiReducer/handleFilterKindReducer";
 import { mapPoints } from "./reducers/uiReducer/mapReducer/mapPoints";
-import { mapPinMode } from "./reducers/uiReducer/mapReducer/mapPinModeReducers.js/mapPinMode";
+import { mapPinMode } from "./reducers/uiReducer/mapReducer/mapPinMode";
 import { mainInput } from "./reducers/uiReducer/mainInputReducer/mainInput";
-import { mapShowPointsMode } from "./reducers/uiReducer/mapReducer/mapShowPointsModeReducers.js/mapShowPointsMode";
-import { currViewInfo } from "./reducers/uiReducer/mapReducer/viewInfoReducers/viewInfo";
-import { currLocation } from "./reducers/uiReducer/mapReducer/mapCurrLocation.js/mapCurrLocation";
+import { mapShowPointsMode } from "./reducers/uiReducer/mapReducer/mapShowPointsMode";
+import { currViewInfo } from "./reducers/uiReducer/mapReducer/viewInfo";
+import { currLocation } from "./reducers/uiReducer/mapReducer/mapCurrLocation";
 
 
 const mapModeReducer = combineReducers({
     pinMode: mapPinMode,
     showPointsMode: mapShowPointsMode,
-    // currViewInfo: currViewInfo,
-    currLocation: currLocation
+    // tooltipExist:tooltipExist,
 })
 
 const mapReducer = combineReducers({

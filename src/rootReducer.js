7cfd983e9eb59995ae5c9efd5f93de1ed6c1d35f
evyatarsh6 <evyash6@gmail.com>
2023-12-25 22:05:46 +0200

    wip

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -7,7 +7,7 @@ import { mainInput } from "./reducers/uiReducer/mainInputReducer/mainInput";
 import { mapShowPointsMode } from "./reducers/uiReducer/mapReducer/mapShowPointsMode";
 import { currViewInfo } from "./reducers/uiReducer/mapReducer/viewInfo";
 import { currLocation } from "./reducers/uiReducer/mapReducer/mapCurrLocation";
-import { tooltipCurrLocat } from "./reducers/uiReducer/mapReducer/mapTooltipExist";
+import { tooltipCurrLocat } from "./reducers/uiReducer/mapReducer/mapTooltipLocation";
 
 
 const mapModeReducer = combineReducers({

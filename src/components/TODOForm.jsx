32c2b58ab259a8de6f5094c189b761a8d0f41144
evyatarsh6 <evyash6@gmail.com>
2024-01-30 20:48:05 +0200

    wip

--- src/components/TODOForm.jsx
+++ src/components/TODOForm.jsx
@@ -11,7 +11,7 @@ import { GetFormDetails } from '../selectors';
 import { addTODO, closeForm, updateForm } from '../actions/actions';
 import { useForm, Controller } from "react-hook-form"
 import { useAddSingleTODO } from '../hooks/useMutateTODOS';
-import { Checkbox } from '@mui/material';
+import { Checkbox, Container, ThemeProvider, createTheme } from '@mui/material';
 import {Autocomplete} from '@mui/material';
 
 export const TODOForm = () => {
@@ -24,6 +24,7 @@ export const TODOForm = () => {
 
     const { handleSubmit, reset, control } = useForm();
     const postSingleTODO = useAddSingleTODO()
+
 
   const handleClose = (event) => {
     event.preventDefault()
@@ -43,6 +44,10 @@ export const TODOForm = () => {
       <Dialog
         open={FormDetails.isFormVisble}
         onClose={handleClose}
+        sx={{
+          position:'fixed',
+          alignItems: 'flex-start'
+        }}
         // PaperProps={{
         //   component: 'form',
         //   onSubmit: (event) => {
@@ -56,7 +61,7 @@ export const TODOForm = () => {
     // }}
       >
         <form onSubmit={handleSubmit(onSubmit)}>
-        <DialogTitle>create new Avi Berger</DialogTitle>
+        <DialogTitle>CREATE NEW AVI BERGER</DialogTitle>
         <DialogContent>
           <DialogContentText margin={1}>
             To create new Avi Berger TODO to this website, enter the wanted info .
@@ -97,8 +102,6 @@ export const TODOForm = () => {
             >
             </Controller>
 
-
-
             <DialogContentText marginTop={5}>
             isChoosen field
             </DialogContentText>
@@ -123,8 +126,6 @@ export const TODOForm = () => {
             </Controller>
 
 
-
-
             <DialogContentText marginTop={5}>
             isDelete field
             </DialogContentText>
@@ -145,152 +146,63 @@ export const TODOForm = () => {
                 id="isChoosen-field"
                 />
               )}
-            >
+              >
+              </Controller> 
+        
+
+            <Container sx={{
+                flexDirection: 'row',
+                textAlign: 'center',
+                justifyContent: 'end',
+                justifyItems:  'flex-end'
+
+            }}>
+              <Controller 
+              control={control}
+              name="submit-btn"
+                 render={({ field: { onBlur ,onChange } }) => (
+              <Button
+              onBlur = {onBlur}
+              onChange = {onChange}
+              size='large'
+              sx = {{
+                marginRight: 3,
+                backgroundColor : 'rgba(25, 118, 210, 0.04)',
+              }}
+              >
+                submit Button
+              </Button>
+                 )}
+              >
+
             </Controller>
-            
-            <Button
-            size='large'
-            sx = {{
-              backgroundColor : 'rgba(25, 118, 210, 0.04)',
-              alignContent: 'flex-end'
-            }}
-            >
-              submit Button
-            </Button>
-            </DialogContent>
 
 
-        {/* <input {...register("firstName", { required: true, maxLength: 20 })} />
-        <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-        <input type="number" {...register("age", { min: 18, max: 99 })} />
-        <input type="submit" />
-        <button onClick={handleClose}> </button>
-        <DialogTitle>create new Avi Berger</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To create new Avi Berger TODO to this website, please enter the wanted info here.
-          </DialogContentText>
-          <TextField
-          {...register("firstName", { required: true, maxLength: 20 })} 
-            autoFocus
-            required
-            margin="dense"
-            id="description-field"
-            name="description"
-            label="description"
-            type="text"
-            fullWidth
-            variant="standard"
-            />
+            <Controller 
+              control={control}
+              name="reset-btn"
+                 render={({ field: { onBlur ,onChange } }) => (
+              <Button
+              onBlur = {onBlur}
+              onChange = {onChange}
+              size='large'
+              sx = {{
+                marginRight: 3,
+                backgroundColor : 'rgba(25, 118, 210, 0.04)',
+              }}
+              >
+                reset Button
+              </Button>
+                 )}
+              >
+
+            </Controller>
+
+
+            </Container>
         </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button type="submit">Subscribe</Button>
-        </DialogActions> */}
         </form>
       </Dialog>
     </React.Fragment>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-   {/* <Dialog
-        open={open}
-        onClose={handleClose}
-        PaperProps={{
-          component: 'form',
-          onSubmit: (event) => {
-            event.preventDefault();
-            const formData = new FormData(event.currentTarget);
-            const formJson = Object.fromEntries(formData.entries());
-            const TODODescription = formJson.TODODescription;
-            console.log(TODODescription);
-            handleClose();
-          },
-        }}
-      >
-        <DialogTitle>new TODO</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To add new Avi Berger to this list, please enter the wanted init info about the task here.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            required
-            margin="dense"
-            id="TODODescription"
-            name="TODODescription"
-            label="TODO description"
-            type="text"
-            fullWidth
-            variant="standard"
-          />
-          <Checkbox
-            autoFocus
-            required
-            margin="dense"
-            id="name"
-            name="email"
-            label="TODO description"
-            type="text"
-            fullWidth
-            variant="standard"
-            />
-
-
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button type="submit">Subscribe</Button>
-        </DialogActions>
-      </Dialog> */}
-
-
-
-
-
-
-
-
-
-            // import React, {useEffect, useMemo, useState } from 'react';
-            // import { useDispatch, useSelector } from 'react-redux';
-            // import {  useForm } from "react-hook-form"
-            // import {Autocomplete} from '@mui/material';
-            // import { GetFormDetails } from '../selectors';
-            // import { closeForm } from '../actions/actions';
-            
-            
-            // export const TODOForm = ({style}) => {
-            //     const { handleSubmit,register, reset } = useForm();
-            //       const TODOKind = useSelector(GetFormDetails)
-            
-            //       const dispatch = useDispatch()
-            
-            //       const onSubmit = (data) => console.log(data)
-            
-            //       const handleCloseForm = event => {
-            //         event.preventDefault()
-            //         dispatch(closeForm())
-            //       }
-                  
-            //       return (
-            //         <form style = {style} onSubmit={handleSubmit(onSubmit)}>
-            //             <input {...register("firstName", { required: true, maxLength: 20 })} />
-            //             <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-            //             <input type="number" {...register("age", { min: 18, max: 99 })} />
-            //             <input type="submit" />
-            //             <button onClick={handleCloseForm}> </button>
-            //             Avi
-            //         </form>
-            //       )
-            //     }

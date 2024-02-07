d6bbc3cbe7686be0be3dbc4f7ef6fd18d461f4b0
evyatarsh6 <evyash6@gmail.com>
2024-02-07 12:40:03 +0200

    wip

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,65 +1,21 @@
-// module.exports = {
-//   root: true,
-//   env: {
-//     node: true,
-//     browser: true,
-//     es2020: true
-// },
-//   extends: [
-//     'eslint:recommended',
-//     'plugin:react/recommended',
-//     'plugin:react/jsx-runtime',
-//     'plugin:react-hooks/recommended',
-//   ],
-//   ignorePatterns: ['dist/' , "node_modules/"],
-//   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-//   settings: { react: { version: '18.2' } },
-//   plugins: ['react-refresh'],
-//   rules: {
-//     'react-refresh/only-export-components': [
-//       'warn',
-//       { allowConstantExport: true },
-//     ],
-//     "no-unused-vars": ["warn", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
-//     "react/prop-types": "off"
-//   },
-// }
-
-
-
 module.exports = {
-  root: true,
   env: {
-    node: true,
     browser: true,
-    es2020: true
+    es2021: true
   },
   extends: [
-    'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'standard'
   ],
-  ignorePatterns: ['dist/', 'node_modules/'],
+  overrides: [
+  ],
   parserOptions: {
-    ecmaVersion: 2022, // Update to a specific version of ECMAScript
+    ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  settings: {
-    react: {
-      version: 'detect' // Use 'detect' to automatically detect the installed React version
-    }
-  },
-  plugins: ['react-refresh'],
+  plugins: [
+    'react'
+  ],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    'no-unused-vars': [
-      'warn',
-      { vars: 'all', args: 'after-used', ignoreRestSiblings: false }
-    ],
-    'react/prop-types': 'off'
   }
-};
+}
